2fd4bfe02ec785e0accaaf0679165fdf8db2080e
Maria <marysiahalamska@gmail,com>
2026-04-17 18:49:07 +0200

    first creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h0>CHAOOS</h0>
       </header>
+
+      <body>
+        <h2>Chaos</h2>
+      </body>
+      <footer>
+        <p>foot</p>
+      </footer>
     </div>
   );
 }
 
 export default App;
+//cd react-chaos
+//npm start
